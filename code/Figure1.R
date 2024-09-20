a49fdbb6c0986acc557e81efc505fb1e08a74cb0
Lindsay Keegan <ltroykeegan@gmail.com>
2024-09-20 14:06:04 -0600

    Update and rename Cdiff_Figure1.R to Figure1.R
    moved to code folder

--- code/Figure1.R
+++ code/Figure1.R
@@ -1,6 +1,6 @@
-source("Scripts/Cdiff_GlobalFunctions.R")
-source("Scripts/Cdiff_LoadData.R")
-source("Scripts/Cdiff_ClusteringData.R")
+source("code/GlobalFunctions.R")
+source("code/LoadData.R")
+source("code/ClusteringData.R")
 
 
 
